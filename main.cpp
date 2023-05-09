523db6556a134a39937d1fc6f4dd3ee4bc3df994
neygenius <mr.neygen@yandex.ru>
2023-05-10 03:01:02 +0400

    Частотный побитовый тест

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <vector>
 #include <bitset>
 #include <random>
+#include <iostream>
 
 std::vector<std::bitset<128>> rand_gen_sequences(size_t count) {
     std::random_device rd;
@@ -23,7 +24,35 @@ std::vector<std::bitset<128>> rand_gen_sequences(size_t count) {
     return sequences;
 }
 
+double frequency_test(const std::bitset<128>& sequence) {
+    int sum = 0;
+
+    for (size_t i = 0; i < sequence.size(); ++i) {
+        if (sequence[i] == 1)
+            sum += 1;
+        else
+            sum += -1;
+    }
+
+    double s_n = std::abs(sum) / std::sqrt(sequence.size());
+    double p_value = std::erfc(s_n / std::sqrt(2));
+
+    return p_value;
+}
+
+
 int main() {
+    size_t sequence_count = 5;
+    std::vector<std::bitset<128>> sequences = rand_gen_sequences(sequence_count);
+
+    for (size_t i = 0; i < sequences.size(); ++i) {
+        const auto& sequence = sequences[i];
+        std::cout << "Sequence " << (i + 1) << ":\n" << sequence << std::endl;
+
+        double test_a = frequency_test(sequence);
+        
+        std::cout << "a) Frequency bitwise test: p_value = " << test_a << "\n" << std::endl;
+    }
 
     return 0;
 }
